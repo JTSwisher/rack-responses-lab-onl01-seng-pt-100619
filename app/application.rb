663fd4eb35593c8edf14e12ed771d641d530e07f
Jeff Swisher <jefftswisher@gmail.com>
2020-01-22 16:16:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,13 @@ class Application
     
     t= Time.now
     resp.write t.hour
- 
+    
+    if t < 12 
+      resp.write "Good Morning!"
+    else 
+      resp.write "Good Afternoon!"
+    end 
+    
     resp.finish
   end
 
